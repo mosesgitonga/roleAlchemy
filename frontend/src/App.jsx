d9c220f6d31207bd35866e2438c4b298bed9d5c5
mosesgitonga <infosec947@gmail.com>
2025-06-06 20:25:49 +0300

    landing page done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+// App.jsx
+import React from 'react';
+import LandingPage from './pages/landing';
+
+function App() {
+  return (
+    <div className="App">
+      <LandingPage />
+    </div>
+  );
+}
+
+export default App;
